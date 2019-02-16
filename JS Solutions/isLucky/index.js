96066b6da144594ce837961a6f3dbd18e07a7443
Ron Forcier II <rforcier2@gmail.com>
2019-02-15 20:29:37 -0500

    Update index.js
    making code more concise

--- JS Solutions/isLucky/index.js
+++ JS Solutions/isLucky/index.js
@@ -5,15 +5,17 @@ isLucky = n => {
         sum2 = 0,
         firstHalf = numSplit.slice(0, num.length/2),
         secondHalf = numSplit.slice(num.length/2, num.length);
+    
     if (num.length % 2 != 0){
         return false;
     }
-    else {
-       for (var i =0; i < firstHalf.length; i++){
+    
+    for (i in firstHalf){
            sum1 += parseInt(firstHalf[i]);
-       }
-        for (var j =0; j < secondHalf.length; j++){
+    }
+    for (j in secondHalf){
            sum2 += parseInt(secondHalf[j]);
-       }
-    }return sum1 == sum2;
+    }
+    return sum1 == sum2;
 }
+
