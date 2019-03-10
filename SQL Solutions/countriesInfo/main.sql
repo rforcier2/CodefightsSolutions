b9d3c09e69996f4e49298805204f1e24f16902dd
rforcier2 <rforcier2@gmail.com>
2019-03-10 12:18:17 -0400

    Updating sql exercises
    More SQL exercises complete from codeSignal

---
+++ SQL Solutions/countriesInfo/main.sql
@@ -0,0 +1,8 @@
+/*Please add ; after each select statement*/
+CREATE PROCEDURE countriesInfo()
+BEGIN
+	  SELECT COUNT(name) AS number,
+           AVG(population) AS average,
+           SUM(population) AS total
+    FROM countries;
+END
